ff9a85d5f8774e5a83a4e5c102e58bb6dad570fb
System Administrator <root@SpaceZzZzzzdeMacBook-Air.local>
2024-01-12 10:53:34 +0800

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphql019c5319": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-<h1>Hello from V2</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//<h1>Hello from V2</h1>
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+
+export default withAuthenticator(App);
+// export default App;
