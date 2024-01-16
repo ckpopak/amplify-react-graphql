92048433d01ac684b69ed335380a80835bb34856
System Administrator <root@SpaceZzZzzzdeMacBook-Air.local>
2024-01-16 15:48:37 +0800

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,51 +1,159 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
+// From: https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/module-two/?e=gs2020&p=build-a-react-app
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
+import { uploadData, getUrl, remove } from 'aws-amplify/storage';
 import {
-  withAuthenticator,
   Button,
+  Flex,
   Heading,
   Image,
+  Text,
+  TextField,
   View,
-  Card,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
 
-function App({ signOut }) {
+// From ChatGPT:
+import {Amplify} from 'aws-amplify';
+import awsExports from './aws-exports'; // The path may vary
+import { generateClient } from 'aws-amplify/api';
+
+import { API, Storage } from 'aws-amplify';
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  TextField,
+  View,
+  withAuthenticator,
+} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsExports);
+
+const client = generateClient();
+
+const App = ({ signOut }) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+async function fetchNotes() {
+  const apiData = await API.graphql({ query: listNotes });
+  const notesFromAPI = apiData.data.listNotes.items;
+  await Promise.all(
+    notesFromAPI.map(async (note) => {
+      if (note.image) {
+        const url = await Storage.get(note.name);
+        note.image = url;
+      }
+      return note;
+    })
+  );
+  setNotes(notesFromAPI);
+}
+ async function createNote(event) {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const image = form.get("image");
+  const data = {
+    name: form.get("name"),
+    description: form.get("description"),
+    image: image.name,
+  };
+  if (!!data.image) await Storage.put(data.name, image);
+  await API.graphql({
+    query: createNoteMutation,
+    variables: { input: data },
+  });
+  fetchNotes();
+  event.target.reset();
+}
+  async function deleteNote({ id, name }) {
+  const newNotes = notes.filter((note) => note.id !== id);
+  setNotes(newNotes);
+  await Storage.remove(name);
+  await API.graphql({
+    query: deleteNoteMutation,
+    variables: { input: { id } },
+  });
+}
   return (
+  <View
+  name="image"
+  as="input"
+  type="file"
+  style={{ alignSelf: "end" }}
+/>
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+      <Heading level={1}>My Notes App</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createNote}>
+    	<Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Note Name"
+            label="Note Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Note Description"
+            label="Note Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
+          <Button type="submit" variation="primary">
+            Create Note
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Notes</Heading>
+      <View margin="3rem 0">
+        {notes.map((note) => (
+  <Flex
+    key={note.id || note.name}
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Text as="strong" fontWeight={700}>
+      {note.name}
+    </Text>
+    <Text as="span">{note.description}</Text>
+    {note.image && (
+      <Image
+        src={note.image}
+        alt={`visual aid for ${notes.name}`}
+        style={{ width: 400 }}
+      />
+    )}
+    <Button variation="link" onClick={() => deleteNote(note)}>
+      Delete note
+    </Button>
+  </Flex>
+))}
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
-}
-
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//<h1>Hello from V2</h1>
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
-//  );
-//}
+};
 
 export default withAuthenticator(App);
-// export default App;
